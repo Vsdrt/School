8ef508dad4c2c1767dd3afdf61145176c9a52297
Mishail Sidorov <U5202urn@yandex.ru>
2026-03-19 03:26:26 +0300

    gdfrgfdg

--- ОШ/chernovic.py
+++
@@ -1,9 +0,0 @@
-stroka = "s"*40 + "k"*28
-
-for i in range(20000):
-	stroka = stroka.replace("kk", "s", 1)
-	stroka = stroka.replace("ss", "kkkk", 1)
-	stroka = stroka.replace("sk", "kkk", 1)
-	if len(stroka) == 85:
-		print(stroka.count("s"))
-		break
